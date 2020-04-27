b1f21068f9ab2b9afd91b7ad4136497e509678ec
Alex Faria <simaofaria11@gmail.com>
2020-04-27 14:24:48 +0100

    add block and return_from

--- src/Exceptional.jl
+++ src/Exceptional.jl
@@ -8,6 +8,28 @@
 
 import Base: error
 
+struct ReturnFromException <: Exception
+    name
+    expr
+end
+
+function block(func, name)
+    try
+        func(name)
+    catch e
+        if e isa ReturnFromException
+            if  e.name == name
+                return e.expr
+            end
+        end
+        rethrow(e)
+    end
+end
+
+function return_from(name, value=nothing)
+    throw(ReturnFromException(name, value))
+end
+
 error(exception::Exception) = throw(exception)
 
 function handler_bind(func, handlers...)
