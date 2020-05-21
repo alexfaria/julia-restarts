4a10654a8b0ab2150283adfb0a1c7157b33b88d4
Alex Faria <simaofaria11@gmail.com>
2020-05-21 22:55:24 +0100

    add very basic user handler of restarts

--- src/ExceptionalExtended.jl
+++ src/ExceptionalExtended.jl
@@ -1,8 +1,29 @@
+current_available_handlers = []
+current_available_handlers = push!(current_available_handlers, [Exception => (c) -> restart_user_handler(c)])
+
+function restart_user_handler(exception)
+    global current_available_restarts
+    if length(current_available_restarts) > 0
+        println()
+        println("#<$(exception)>#")
+        println(" [Condition of type $(typeof(exception))]")
+        println("Restarts:")
+        i = 1
+        for restart in current_available_restarts
+            println(" $(i): [$(uppercase(String(restart[1])))] $(restart[1])")
+            i += 1
+        end
+
+        selected_restart = readline()
+        i = tryparse(Int, selected_restart)
+        invoke_restart(current_available_restarts[i][1])
+    end
+end
+
 function signal(exception::Exception)
     global current_available_handlers
 
     for handler_group in current_available_handlers
-        #TODO: descobrir se o signal so corre o primeiro handler
         for handler in handler_group
             if exception isa handler[1]
                 handler[2](exception)
